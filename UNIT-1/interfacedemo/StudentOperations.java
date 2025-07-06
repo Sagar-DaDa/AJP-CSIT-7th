f6b20299b4c574bfb4632ef565f0aae19b0388a6
Sagar Shrestha <156745194+Sagar-DaDa@users.noreply.github.com>
2025-07-06 16:45:36 +0545

    Update StudentOperations.java

--- UNIT-1/interfacedemo/StudentOperations.java
+++ UNIT-1/interfacedemo/StudentOperations.java
@@ -1,55 +1,29 @@
-{
-  "name": "is-number",
-  "description": "Returns true if a number or string value is a finite number. Useful for regex matches, parsing, user input, etc.",
-  "version": "7.0.0",
-  "homepage": "https://github.com/jonschlinkert/is-number",
-  "author": "Jon Schlinkert (https://github.com/jonschlinkert)",
-  "contributors": [
-    "Jon Schlinkert (http://twitter.com/jonschlinkert)",
-    "Olsten Larck (https://i.am.charlike.online)",
-    "Rouven Weßling (www.rouvenwessling.de)"
-  ],
-  "repository": "jonschlinkert/is-number",
-  "bugs": {
-    "url": "https://github.com/jonschlinkert/is-number/issues"
-  },
-  "license": "MIT",
-  "files": [
-    "index.js"
-  ],
-  "main": "index.js",
-  "engines": {
-    "node": ">=0.12.0"
-  },
-  "scripts": {
-    "test": "mocha"
-  },
-  "devDependencies": {
-    "ansi": "^0.3.1",
-    "benchmark": "^2.1.4",
-    "gulp-format-md": "^1.0.0",
-    "mocha": "^3.5.3"
-  },
-  "keywords": [
-    "cast",
-    "check",
-    "coerce",
-    "coercion",
-    "finite",
-    "integer",
-    "is",
-    "isnan",
-    "is-nan",
-    "is-num",
-    "is-number",
-    "isnumber",
-    "isfinite",
-    "istype",
-    "kind",
-    "math",
-    "nan",
-    "num",
-    "number",
-    "numeric",
-    "parseFloat",
-    "parseI
+public class StudentOperations implements AccountOperations {
+  public void showMessage() {
+    System.out.println("This is Student Operation Class.");
+  }
+
+  @Override
+  public void viewTotalFee(){
+    System.out.println("Total Fee: 500000");
+  };
+
+  @Override
+  public void paidFee(){
+    System.out.println("Paid Fee: 260000");
+  };
+
+  @Override
+  public void remainingFee(){
+    System.out.println("Remaining Fee: 240000");
+  };
+
+  public static void main(String[] args) {
+    StudentOperations obj = new StudentOperations();
+    obj.viewTotalFee();
+    obj.paidFee();
+    obj.remainingFee();
+    obj.showMessage();
+  }
+
+}
